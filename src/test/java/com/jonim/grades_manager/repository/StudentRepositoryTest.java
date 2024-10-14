c26e0c2f57faf32bc290ce4fa5ba27a324bd3842
JoniMonetti1 <158229526+JoniMonetti1@users.noreply.github.com>
2024-10-14 02:10:02 -0300

    Student repository tests implement

--- src/test/java/com/jonim/grades_manager/repository/StudentRepositoryTest.java
+++ src/test/java/com/jonim/grades_manager/repository/StudentRepositoryTest.java
@@ -1,15 +1,19 @@
 package com.jonim.grades_manager.repository;
 
-import static org.assertj.core.api.Assertions.*;
-
 import com.jonim.grades_manager.models.Student;
 import com.jonim.grades_manager.repositories.StudentRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @ActiveProfiles("test")
@@ -18,6 +22,16 @@ public class StudentRepositoryTest {
 
     @Autowired
     private StudentRepository studentRepository;
+
+    private Student student0;
+
+    @BeforeEach
+    public void setUp() {
+        student0 = Student.builder()
+                .name("joni")
+                .surname("monetti")
+                .build();
+    }
 
     @Test
     public void shouldSaveStudent() {
@@ -33,5 +47,73 @@ public class StudentRepositoryTest {
         //Then - se espera el resultado esperado
         assertThat(savedStudent).isNotNull();
         assertThat(savedStudent.getStudentId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void shouldReturnStudentList() {
+        Student student1 = Student.builder()
+                .name("Joni")
+                .surname("Miller")
+                .build();
+
+        Student student2 = Student.builder()
+                .name("Alice")
+                .surname("Johnson")
+                .build();
+
+        studentRepository.save(student1);
+        studentRepository.save(student2);
+
+        //When - accion o comportamiento a probar
+        List<Student> studentList = studentRepository.findAll();
+
+        assertThat(studentList).isNotNull();
+        assertThat(studentList.size()).isEqualTo(2);
+        assertThat(studentList).extracting(Student::getName)
+                .containsExactlyInAnyOrder("Joni", "Alice");
+    }
+
+    @Test
+    public void shouldReturnAStrudentById() {
+        //given
+        studentRepository.save(student0);
+
+        //when
+        Student student = studentRepository.findById(student0.getStudentId()).get();
+
+        //Then
+        assertThat(student).isNotNull();
+        assertThat(student.getName()).isEqualTo(student0.getName());
+        assertThat(student.getSurname()).isEqualTo(student0.getSurname());
+    }
+
+    @Test
+    public void shouldUpdateAStudent() {
+        //given
+        studentRepository.save(student0);
+
+        //when
+        Student savedStudent = studentRepository.findById(student0.getStudentId()).get();
+        savedStudent.setName("abel");
+        savedStudent.setSurname("Monettii");
+        Student updatedStudent = studentRepository.save(savedStudent);
+
+        //then
+        assertThat(updatedStudent).isNotNull();
+        assertThat(updatedStudent.getName()).isEqualTo("abel");
+        assertThat(updatedStudent.getSurname()).isEqualTo("Monettii");
+    }
+
+    @Test
+    public void shouldDeleteAStudentById() {
+        //given
+        studentRepository.save(student0);
+
+        //when
+        studentRepository.deleteById(student0.getStudentId());
+        Optional<Student> optionalStudent = studentRepository.findById(student0.getStudentId());
+
+        //then
+        assertThat(optionalStudent).isEmpty();
     }
 }
